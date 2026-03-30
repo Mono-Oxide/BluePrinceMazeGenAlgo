1ef6b102bfe2d5f3c778154379cfd6aeb022287c
Mono-Oxide <dvdb422@gmail.com>
2026-03-30 21:13:20 +0200

    R O T A T E

--- BluePrinceThingy/Program.cs
+++ BluePrinceThingy/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 public class Maze
 {
@@ -21,15 +22,27 @@ public enum Wall
 }
 
 public class Pathway {
-public Dictionary<string, Wall[]> pathwayTypes = new Dictionary<string, Wall[]>
-{
-    {"dead end", [Wall.North, Wall.East, Wall.South]} ,
-    {"4-way", []},
-    {"straight", [Wall.East, Wall.West]},
-    {"turn-L", [Wall.North, Wall.East]},
-    {"turn-R", [Wall.North, Wall.West]},
-    {"t-ur", [Wall.West]},
-    {"t-lr", [Wall.North]},
-    {"t-ul", [Wall.East]},
-};
+
+    public Dictionary<string, Wall[]> pathwayTypes = new Dictionary<string, Wall[]>
+    {
+        {"dead end", [Wall.North, Wall.East, Wall.South]},
+        {"4-way", []},
+        {"straight", [Wall.East, Wall.West]},
+        {"turn-L", [Wall.North, Wall.East]},
+        {"turn-R", [Wall.North, Wall.West]},
+        {"t-ur", [Wall.West]},
+        {"t-lr", [Wall.North]},
+        {"t-ul", [Wall.East]},
+    }; 
+
+    public Wall[] Rotate(Wall[] original, int turnAmount)
+    {
+        Wall[] result = new Wall[original.Length];
+        for (int i = 0; i < original.Length; i++)
+        {
+            result[i] = (Wall)(((int)original[i] + turnAmount) % 4);
+        }
+        return result;
+    }
+    
 }
