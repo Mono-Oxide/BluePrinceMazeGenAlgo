d9503c939b35b981e5e4d06c545ab3d92a7ce86b
Mono-Oxide <dvdb422@gmail.com>
2026-04-03 01:29:09 +0200

    i didn't docu"meant" to "DO"coment that.... HAHAHAHAHAHA FUCKING LAUGH!!!!

--- BluePrinceThingy/obj/Debug/net10.0/BluePrinceThingy.AssemblyInfo.cs
+++ BluePrinceThingy/obj/Debug/net10.0/BluePrinceThingy.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BluePrinceThingy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0167db0f2f42413dc6ee7713e6106c40322d5bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4e3a51fcdfbf9100e12c599725815a5056d565b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BluePrinceThingy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BluePrinceThingy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
