838e67601731c26d5946493a18e58fc86c446e75
Mono-Oxide <dvdb422@gmail.com>
2026-04-03 15:35:01 +0200

    schmebulock

--- BluePrinceThingy/obj/Debug/net10.0/BluePrinceThingy.AssemblyInfo.cs
+++ BluePrinceThingy/obj/Debug/net10.0/BluePrinceThingy.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BluePrinceThingy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bc9b12a7f1e55861111443bae8700c108fc782c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06b5369201357abccd4198b3378285e582231e1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BluePrinceThingy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BluePrinceThingy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
