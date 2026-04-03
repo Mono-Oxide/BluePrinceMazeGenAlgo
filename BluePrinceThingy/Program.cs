4b44ba516d2ed28337ae07473d190b0f25efc6a0
RandomGuy <tigoschippers@gmail.com>
2026-04-03 15:41:31 +0200

    should work

--- BluePrinceThingy/Program.cs
+++ BluePrinceThingy/Program.cs
@@ -481,6 +481,8 @@ public class Agents
             }
             foreach (Point move in moves)
             {
+                if (0 > curX || curX >= maze.GetLength(0) || 0 > curY || curY >= maze.GetLength(1))
+                    continue;
                 Node toAdd = maze[move.X, move.Y];
                 if (!toExpand.Contains(toAdd) && !expanded.Contains(toAdd))
                     toExpand.Add(toAdd);
